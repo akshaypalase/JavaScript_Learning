4b077d74213ba2af55f97aefdb10bf3299587cd7
akshaypalase <akshaypalase3@gmail.com>
2022-10-04 20:29:15 +0530

    First program execution in JS
    First program execution in JS

---
+++ demo.js
@@ -0,0 +1,12 @@
+// this is my first code
+console.log("Akshay Arun Palase ");
+console.log("playing cricket");
+console.log("playing Vollyball");
+console.info("Listening to Music");
+console.info("positive thinker");
+console.info("eager to learn new skills");
+console.warn("Trust everyone easily");
+console.warn("I can not say no if anyone asks for help");
+console.warn("i know basic excel trying to improve it");
+console.log(30+50);
+console.log("49*479");
