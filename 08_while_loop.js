ae4df2e896209038d7d2b23a5dd8ef6ec15900f1
akshaypalase <akshaypalase3@gmail.com>
2022-10-15 00:22:45 +0530

    updated
    updated

---
+++ 08_while_loop.js
@@ -0,0 +1,18 @@
+console.log(`===================Print numbera from 5 to 15 by incrementing 1===================`);
+var num=5;
+while(num<=15){
+      console.log(`number:-${num}`);
+      num=num+1;
+}
+console.log(`===================Print numbera from 50 to 40 by decrementing 1===================`);
+var num=50;
+while(num>=40){
+      console.log(`number:-${num}`);
+      num=num-1;
+}
+console.log(`===================Find first 15 odd numbers===================`);
+var odd=1;
+while(odd>=30){
+      console.log(`number:-${odd}`);
+      odd=odd+2;
+}
